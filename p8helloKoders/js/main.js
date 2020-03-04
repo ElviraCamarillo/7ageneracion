1d869789c9406dd3b532e06688d17aa9217d2099
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-03-04 15:44:57 -0600

    [ADD] Exercise js

---
+++ p8helloKoders/js/main.js
@@ -0,0 +1,36 @@
+/*var string = "una cadena de caracteres"
+var number = 4;
+var object = {
+    name:"some name",
+    age: 30,
+    phone:
+}
+
+array=[
+    "koder a",
+    "koder b",
+    "koder c"
+]
+
+console.log("Hello Koders!!!")
+console.warn("Aware Koders!!!")
+console.error("Noooo Koders!!!")
+
+1.- Crear variables x,y (asignar valor númerico)
+2.- Asignar valor númerico
+3. Suma
+4.- Resta
+Multiplicación
+División*/
+
+var x = 5;
+var y = 3;
+
+console.log("La suma de x + y es igual a ",
+    x + y)
+console.log("La resta de x - y es igual a ",
+    x - y)
+console.log("La multiplicación de x * y es igual a ",
+    x * y)
+console.log("La división de x / y es igual a ",
+    x / y)
