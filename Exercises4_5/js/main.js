73c16c6f85f9bdbcfc27a7b331b6191b7d553f2f
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-03-05 19:22:31 -0600

    [ADD] Practice 4 y 5

---
+++ Exercises4_5/js/main.js
@@ -0,0 +1,44 @@
+Practica 4
+//Recibir del usuario los datos de 3 lados de un triangulo
+//Con base en esos datos, determinar el área de un triángulo y mostrar el mensaje.
+//En otro mensaje, indicar si el triángulo es isósceles, equilátero o escaleno.
+
+/*
+Declarar la variable a
+Declarar la variable b
+Declarar variable c
+
+Solicitar al usuario “ingresé un número” y asignar el valor a la variable  a
+Solicitar al usuario “ingresé un número” y asignar el valor a la variable b
+Solicitar al usuario “ingresé un número” y asignar el valor a la variable c
+
+Determinar si a, b, y forman un triángulo, comparando que la suma de dos lados sea mayor que el tercer lado en todos los casos.
+    En caso de que no se pueda formar un triángulo imprimir mensaje “Con estos números no podemos formar un triángulo, Gracias.”  En caso de que si se pueda continuar con el proceso.
+
+Convertir el tipo de la variable  “a”  a variable tipo número y asignar el reasignarlas a las variables correspondientes.
+Convertir el tipo de la variable  “b”  a variable tipo número y asignar el reasignarlas a las variables correspondientes.
+Convertir el tipo de la variable  “b”  a variable tipo número y asignar el reasignarlas a las variables correspondientes.
+
+Declarar la variable  semiperimeter
+Asignar el valor de la variable semiperimeter = (a + b +c) / 2
+
+Declarar la variable triangleArea
+Asignar el valor de la variable triangleArea=raíz-cuadrada( semiperimeter(semiperimeter -a)( semiperimeter -b)( semiperimeter -c))
+Imprimir el mensaje “El área del triángulo es:” concatenado con el valor de la variable triangleArea.
+
+Determinar qué tipo de triángulo es, comparando sus lados de la siguiente la forma:
+    Si a=b=c imprimir “Es un triángulo equilátero.”
+    Sino comparar si (a=b | b=c| a=c) e imprimir “Es un triángulo isósceles.”
+    Sino se cumplen ninguna de las condiciones imprimir el mensaje “Es un triángulo escaleno.”
+
+Declarar la variable hypotenuse
+
+Determinar cuál es la hipotenusa, realizando el siguiente proceso:
+    Si (a>b & a>c) entonces imprimir mensaje “La hipotenusa es el lado:” + valor variable a  
+    Sino comprobar si (b >a & b>c) entonces imprimir mensaje “La hipotenusa es el lado:” + valor variable b 
+    En caso de que no se cumpla ninguna de estas condiciones imprimir mensaje “La hipotenusa es el lado:” + valor variable c
+
+Asignar el valor de la hipotenusa a la variable hypotenuse
+
+
+*/
