ca74b230b4f0a32dd05d25573568f29b1b919b21
Elvira-camarillo <gcamarilloluna@gmail.com>
2020-04-23 19:23:03 -0500

    ADD HomeWork

--- NodeJS/my-first-api/index.js
+++ NodeJS/my-first-api/index.js
@@ -17,30 +17,56 @@ server.get('/', (request,response)=>{
 })
 
 //Section koders 
+
+//query parameters
+//http://servidor/ruta/del/recurso?
+//GET http://api.kodemia.mx/koders?limit=66
+
+// Querry  parameters
 server.get('/koders',(request,response) => {
+    const{ limit=10} =request.query
     response.json({
         message:'All the koders',
         data:{
-            koders:kodemia.koders
+            koders:kodemia.koders.slice(0,parseInt(limit))
+            
         }
 })
 })
 
 server.post('/koders',(request,response) => {
-    const newKoder = {
-        name:request.body.name,
-        generation:request.body.generation
-    }
-    kodemia.koders.push(newKoder)
+    const { name, generation } = request.body
+    kodemia.koders.push({
+        name,
+        generation
+    })
     response.json({
         message:'new koder added',
         data:{
-            koder:newKoder
+            koder:{
+                name,
+                generation
+            }
         }
     })
 })
 
-//Sectio a-Mentors
+// server.post('/koders',(request,response) => {
+    
+//     const newKoder = {
+//         name:request.body.name,
+//         generation:request.body.generation
+//     }
+//     kodemia.koders.push(newKoder)
+//     response.json({
+//         message:'new koder added',
+//         data:{
+//             koder:newKoder
+//         }
+//     })
+// })
+
+//Section a-Mentors
 server.get('/amentors',(request,response)  => {
     response.json({
         message:'All the aMentors',
@@ -66,9 +92,43 @@ server.post('/amentors',(request,response)  => {
 
 })
 
+// uri parameters
+// GET /koders/[name]
+//GET /koders/Charles -> name=charles
+
+// server.get('/koders/:name',(request,response)=>{
+//     response.json({
+//         name:request.params.name
+//     })
+// })
+
+
+//Practica
+//Un endpoit que reciba el  indice  de un koder  y regresa  un  koder que se  encuentra en ese ídice
+//GET  /koders/:indice-> indice = 0  ... kodemia.koders[0]
+server.get('/koders/:index',(request,response)=>{
+    const {index} =request.params
+    response.json({
+        message:'Index Koder',
+        koder:  kodemia.koders[index]
+    })
+
+})
+
+
+
 server.listen(8081,()=>{
     console.log('Server is running')
 })
+
+
+
+
+
+
+
+
+
 
 //200.1.2.10
 ///Users/gabrielaelviracamarilloluna/Documents/kodemia/septima-gen/my-firts-api
